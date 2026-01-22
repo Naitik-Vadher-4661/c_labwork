b9f3a36f543e8dd9ce822d7a9f4d2999e4651025
Naitik-Vadher-4661 <vnaitik767@gmail.com>
2026-01-22 23:44:40 +0530

    new

--- 25cp016_C_LAB/Practice_set_1/2.c
+++ 25cp016_C_LAB/Practice_set_1/2.c
@@ -4,4 +4,5 @@ int a;
 printf("put any value  here:");
 scanf("%d",&a);                         //this line takes input from user
 printf("the value is here:%d",a);          //this line prints the value given by user
+
 }
