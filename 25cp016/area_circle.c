7a7daad4e343ae49457f3eeee2cc171d5e687965
Naitik-Vadher-4661 <vnaitik767@gmail.com>
2026-01-22 23:20:37 +0530

    Delete area_circle.c

--- 25cp016/area_circle.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-float radius;
-printf("put value of Radius here:\n");
-scanf("%f",&radius);
-printf("the area of circle is :%.2f\n",3.14*radius*radius);
-printf("the circumference of circle is:%.2f\n",2*3.14*radius);
-}
