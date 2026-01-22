3420da7c8385fc5209b4619b6a1b42dd4c138644
Naitik-Vadher-4661 <vnaitik767@gmail.com>
2026-01-22 23:18:46 +0530

    Delete 2.c

--- 25cp016/2.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void main(){
-int a;
-printf("put any value  here:");
-scanf("%d",&a);                         //this line takes input from user
-printf("the value is here:%d",a);          //this line prints the value given by user
-}
